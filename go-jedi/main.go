a19ba3b3ea0bf8b038ee1f336656f1358232c1e0
tuankg1028 <lethanhtuan1028@gmail.com>
2024-07-23 09:27:57 +0700

    feat: add more testcase

--- go-jedi/main.go
+++ go-jedi/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
 	"jedi"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -18,34 +20,148 @@ var TestHierarchy = []byte("testHierarchy")
 const quote1 = "Imagination is more important than knowledge. --Albert Einstein"
 const quote2 = "Today is your day! / Your mountain is waiting. / So... get on your way! --Theodor Seuss Geisel"
 
+type DecryptRequest struct {
+	URI              string `string:"uri" binding:"required"`
+	ENCRYPTEDMESSAGE string `string:"encryptedMessage" binding:"required"`
+	KEY              string `string:"key" binding:"required"`
+}
+
+type EncryptRequest struct {
+	URI     string `string:"uri" binding:"required"`
+	MESSAGE string `string:"message" binding:"required"`
+}
+
 func main() {
+	data := TestHierarchy
+
+	sEnc := base64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+
+	sDec, _ := base64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
 	ctx := context.Background()
 	_, store := NewTestKeyStore()
 	encoder := jedi.NewDefaultPatternEncoder(TestPatternSize - jedi.MaxTimeLength)
 
-	// state := NewTestState()
-	// now := time.Now()
+	state := NewTestState()
+	now := time.Now()
 
 	// testMessageTransfer(state, TestHierarchy, "a/b/c", now, quote1)
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/jedi-private-key", func(c *gin.Context) {
 		uri := "a/b/c"
+
 		start := time.Unix(1565119330, 0)
 		end := time.Unix(1565219330, 0)
+
+		parent := c.DefaultQuery("parent", "")
+		if parent != "" {
+			// delegation := new(jedi.Delegation)
+
+			// if !delegation.Unmarshal([]byte(parent)) {
+			// 	fmt.Println("Could not unmarshal delegation")
+			// }
+
+			// fmt.Println(1)
+			// fmt.Println(delegation)
+
+			// c.JSON(200, gin.H{
+			// 	"data": "Test",
+			// })
+			uri := "a/b/c/d"
+			delegation, err := jedi.Delegate(ctx, store, encoder, TestHierarchy, uri, start, end, jedi.DecryptPermission|jedi.SignPermission)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			marshalled := delegation.Marshal()
+
+			c.JSON(200, gin.H{
+				"data": marshalled,
+			})
+			return
+		}
+
 		delegation, err := jedi.Delegate(ctx, store, encoder, TestHierarchy, uri, start, end, jedi.DecryptPermission|jedi.SignPermission)
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		marshalled := delegation.Marshal()
 
 		c.JSON(200, gin.H{
 			"data": marshalled,
 		})
 	})
+
+	r.POST("/encrypt", func(c *gin.Context) {
+		var err error
+
+		var encryptRequest EncryptRequest
+		c.BindJSON(&encryptRequest)
+
+		message := encryptRequest.MESSAGE
+		uri := encryptRequest.URI
+
+		var encrypted []byte
+		if encrypted, err = state.Encrypt(ctx, TestHierarchy, uri, now, []byte(message)); err != nil {
+			fmt.Println(err)
+		}
+
+		var decrypted []byte
+		if decrypted, err = state.Decrypt(ctx, TestHierarchy, uri, now, encrypted); err != nil {
+			fmt.Println(err)
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if !bytes.Equal(decrypted, []byte(message)) {
+			fmt.Println("Original and decrypted messages differ")
+		}
+
+		c.JSON(200, gin.H{
+			"data": base64.StdEncoding.EncodeToString(encrypted),
+		})
+	})
+
+	r.POST("/decrypt", func(c *gin.Context) {
+		var err error
+		var decryptRequest DecryptRequest
+		c.BindJSON(&decryptRequest)
+
+		encrypted, err := base64.StdEncoding.DecodeString(decryptRequest.ENCRYPTEDMESSAGE)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		uri := decryptRequest.URI
+
+		var decrypted []byte
+		if decrypted, err = state.Decrypt(ctx, TestHierarchy, uri, now, encrypted); err != nil {
+			fmt.Println(err)
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if !bytes.Equal(decrypted, []byte("test message")) {
+			fmt.Println("Original and decrypted messages differ")
+		}
+
+		str := string(decrypted)
+		fmt.Println(decrypted) // Output: abc
+
+		c.JSON(200, gin.H{
+			"data": str,
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 	fmt.Println("DONE!")
-
 }
 
 type TestPublicInfo struct {
--- kyc-contract/test/MedicalRecord-test.js
+++ kyc-contract/test/MedicalRecord-test.js
@@ -3,7 +3,7 @@ const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { expect, use } = require("chai");
 const crypto = require('crypto');
 const {ethers, deployments, getNamedAccounts, getUnnamedAccounts} = require("hardhat")
 const axios = require('axios');
@@ -18,7 +18,7 @@ const JediInstance = axios.default.create({
   baseURL: "http://localhost:8080"
 })
 
-describe("KYC contract test", async function () {
+describe("JEDI Blockchain for Medical record TEST", async function () {
   var owner, otherAccount, contract, pinata
   before(async () => {
     await loadFixture(
@@ -46,8 +46,7 @@ describe("KYC contract test", async function () {
 
 
 
-
-  const createUser = async (userInfo) => {
+  const pinMetadataToIPFS = async (metadata) => {
     const options = {
       pinataMetadata: {
         name: "metadata.json",
@@ -56,59 +55,136 @@ describe("KYC contract test", async function () {
         cidVersion: 0,
       },
     };
-  
-    const payload = {
-      data: userInfo
-    }
-    const { IpfsHash } = await pinata.pinJSONToIPFS(payload, options);
-    await contract.mint(TOKEN_ID++, IpfsHash);
 
-    // Generate an RSA key pair
-    const key = new NodeRSA({ b: 512 }); // You can adjust the key size as needed
-    const rsaPublicKey = key.exportKey('public');
-    const rsaPrivateKey = key.exportKey('private');
+    const { IpfsHash } = await pinata.pinJSONToIPFS(metadata, options);
 
-    return { nftURI: IpfsHash, rsaPublicKey, rsaPrivateKey }
+    return IpfsHash
+  }
+  // pin the metadata to IPFS create a nft on blockchain and 
+  const createNFT = async (IpfsHash) => {
+    const nft = await contract.mint(TOKEN_ID++, IpfsHash);
+
+    return nft
   }
 
 
+  const userInfoA = {
+    fullName: "patient A",
+    address: "address 1",
+    age: 20,
+  }
 
-  it("Create user A and delegate B", async function () {
-    const userA = await createUser({
-      fullName: "User A",
-      address: "Address 1",
-    })
+  it("Step 1: Generate RSA keys and create NFT for storing KYC on blockchain", async function () {
+    const key = new NodeRSA({ b: 512 }); 
+
+    // Generate an RSA key pair
+    const rsaPrivateKey = key.exportKey('private');
+    const rsaPublicKey = key.exportKey('public');
+
+    // Pin data to IPFS
+    const IpfsHash = await pinMetadataToIPFS({
+      data: {
+        ...userInfoA,
+        rsaPublicKey: rsaPublicKey
+      }
+    });
+
+    expect(IpfsHash).to.be.a('string')
+
+    // Create NFT
+    const nft = await createNFT(IpfsHash)
+    
+    expect(nft).to.be.a('object')
+    expect(nft).to.have.property('hash')
+    expect(nft.hash).to.be.a('string')
+
+
+    // save info for next steps
+    userInfoA.rsaPublicKey = rsaPublicKey
+    userInfoA.rsaPrivateKey = rsaPrivateKey
+ 
+
+    // return data to user
+    return {
+      rsaPrivateKey
+    }
+  });
+
+  it("Step 2.1: Delegate JEDI key for user A and encrypt it with RSA key", async function () {
     // generate jedi private key
     const jediPKResA = await JediInstance.get(`/jedi-private-key/`)
-    const jediPrivateKeyA = jediPKResA.data.result
-    expect(userA).to.be.a('object');
-    console.log({userA: {
-      ...userA,
-      jediPrivateKey: jediPrivateKeyA
-    }})
+    const jediKeys = jediPKResA.data.data
 
+    expect(jediKeys).to.be.a('string')
 
-    const userB = await createUser({
-      fullName: "User B",
-      address: "Address B",
-    })
+   
 
-    // delete user B
-    const jediPKResB = await JediInstance.get(`/jedi-private-key/?parent=${jediPrivateKeyA}`)
-    const jediPrivateKeyB = jediPKResB.data.result
+    const publicKeyEncryption = new NodeRSA(userInfoA.rsaPublicKey);
+    const encryptedPublickey = publicKeyEncryption.encrypt(jediKeys)
+
+    // save info for next steps
+    userInfoA.encryptedJediKey = encryptedPublickey
+
+    // return data to user
+    return {
+      encryptedPublickey
+    }
+  })
+
+  it("Step 2.2: Decrypt encrypted jedi keys", async function () {
+    const privateKeyDecryption = new NodeRSA(userInfoA.rsaPrivateKey);
+    const jediKeys = privateKeyDecryption.decrypt(userInfoA.encryptedJediKey, 'utf8');
+
+     // save info for next steps
+      userInfoA.jediKeys = jediKeys
+
+    // return data to user
+    return {
+      jediKeys
+    }
+  })
+
+  it("Step 3: Delegate JEDI key for user B from user A", async function () {
+    const jediKeyRes = await JediInstance.get(`/jedi-private-key?parent=${userInfoA.jediKeys}`)
+
+    expect(jediKeyRes.data).to.be.a('object')
+    expect(jediKeyRes.data).to.have.property('data')
+    expect(jediKeyRes.data.data).to.be.a('string')
+
+    const jediKeys = jediKeyRes.data.data
+    return {
+      jediKeys
+    }
+  })
+  
+
+  it("Encrypt and Decrypt message with JEDI", async function () {
+    const message = "Hello world"
     
-    const publicKeyEncryption = new NodeRSA(userB.rsaPublicKey);
-    const privateKeyDecryption = new NodeRSA(userB.rsaPrivateKey);
+    const encryptedMsgRes = await JediInstance.post(`/encrypt`, {
+      message,
+      uri: "a/b/c/d",
+    })
+    
+    expect(encryptedMsgRes).to.be.a('object')
+    expect(encryptedMsgRes).to.have.property('data')
+    expect(encryptedMsgRes.data.data).to.be.a('string')
 
-    const encryptedMessage = publicKeyEncryption.encrypt(jediPrivateKeyB)
-    const decryptedMessage = privateKeyDecryption.decrypt(encryptedMessage, 'utf8');
+    const encryptedMsg = encryptedMsgRes.data.data
 
+    const decryptedMsgRes = await JediInstance.post(`/decrypt`, {
+      encryptedMessage: encryptedMsg,
+      uri: "a/b/c/d",
+      key: userInfoA.jediKeys
+    })
+    
+    expect(decryptedMsgRes).to.be.a('object')
+    expect(decryptedMsgRes).to.have.property('data')
+    expect(decryptedMsgRes.data.data).to.be.a('string')
 
-    console.log({userB: {
-      ...userB,
-      jediPrivateKey: decryptedMessage
-    }})
-  });
+    const decryptedMsg = decryptedMsgRes.data.data
+    expect(decryptedMsg).to.be.eq(message)
+  })
 });
 
 
